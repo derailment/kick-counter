c5a3a2e48694e3df02cb8f7299c90de1d45df217
Cooper Chang <cpstkd1@gmail.com>
2019-12-22 13:51:09 +0800

    不知道有沒有做對

--- app/src/main/java/com/example/kick_counter/Player.java
+++ app/src/main/java/com/example/kick_counter/Player.java
@@ -3,8 +3,11 @@ package com.example.kick_counter;
 public class Player {
     private int  playOne;
     private int playerTwo;
+    private int Score;
 
-
+    public void setScore (int Score){
+        this.Score = Score;
+    }
     public void setPlayOne (int playOne){
         this.playOne = playOne;
     }
@@ -12,5 +15,6 @@ public class Player {
     public void setplayerTwo(int playerTwo){
         this.playerTwo = playerTwo;
     }
+
 
 }
